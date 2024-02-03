8591dc7f2827a8ab858bbed6c7dda9c0a194f6ef
Shivesh Gulati <shivesh21286@iiitd.ac.in>
2024-02-04 01:24:43 +0530

    Started with implementation of server and client (seller)

--- Part-1/client_seller.py
+++ Part-1/client_seller.py
@@ -2,3 +2,15 @@ import grpc
 import market_seller_pb2_grpc
 import market_seller_pb2
 
+def run():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = market_seller_pb2_grpc.MarketPlaceStub(channel)
+        print("Welcome to the Shop Seller :-")
+        print("Here are your possible options : -")
+        print("1) Register yourself as a seller")
+        print("2) Sell item")
+        print("3) Update item record")
+        print("4) Delete item")
+        print("5) Display your listed items")
+
+
--- Part-1/server_market_place.py
+++ Part-1/server_market_place.py
@@ -1,3 +1,19 @@
 import grpc
 import market_seller_pb2
 import market_seller_pb2_grpc
+
+class MarketPlaceService(market_seller_pb2_grpc.MarketPlaceServicer):
+    def RegisterSeller(self, request, context):
+        return super().RegisterSeller(request, context)
+
+    def SellItem(self, request, context):
+        return super().SellItem(request, context)
+    
+    def UpdateItem(self, request, context):
+        return super().UpdateItem(request, context)
+    
+    def DeleteItem(self, request, context):
+        return super().DeleteItem(request, context)
+    
+    def DisplaySellerItems(self, request, context):
+        return super().DisplaySellerItems(request, context)
