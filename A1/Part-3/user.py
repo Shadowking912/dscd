eabf7b3e8171195d08aeb82daf0a4be2411ea2b5
Shivesh Gulati <shivesh21286@iiitd.ac.in>
2024-02-09 23:50:32 +0530

    Started with Notification Service

--- A1/Part-3/user.py
+++ A1/Part-3/user.py
@@ -2,6 +2,8 @@ import pika
 import json
 import sys
 
+
+    
 def updateSubscription(channel, user_name,youtuber_name, subscribe):
     channel.queue_declare(queue='user_requests')
     message = {
@@ -15,22 +17,21 @@ def updateSubscription(channel, user_name,youtuber_name, subscribe):
     else:
         print("Logged In")
 
-def receiveNotifications(self):
-    def callback(ch, method, properties, body):
-        print("New Notification:", body.decode('utf-8'))
+def receiveNotifications(username,connection):
+    def callback(ch,method,properties,body):
+        body = body.decode('utf-8')
+        print(body)
 
-    self.channel.basic_consume(queue=self.user_name, on_message_callback=callback, auto_ack=True)
-    print("Waiting for notifications...")
-    self.channel.start_consuming()
-
+    channel2  = connection.channel()
+    channel2.basic_consume(queue=username,on_message_callback=callback,auto_ack=True)
+    channel2.start_consuming()
+    
 if __name__ == "__main__":
     if len(sys.argv)==2:
         username=sys.argv[1]
         connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-        channel = connection.channel()
-        updateSubscription(channel,username,"","")
-        receiveNotifications(username)
-        print("typ1")
+        updateSubscription(connection.channel(),username,"","")
+        receiveNotifications(username,connection)
 
     elif len(sys.argv)==4:
         username=sys.argv[1]
@@ -38,10 +39,9 @@ if __name__ == "__main__":
         youtuber_name = sys.argv[3]
         connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
         channel = connection.channel()
-        updateSubscription(channel,username,option,youtuber_name)
-        print("typ2")
+        updateSubscription(channel,username,youtuber_name,option)
+        
     else:
         print("Invalid option")
-    updateSubscription(channel,username,"","")
     
 
