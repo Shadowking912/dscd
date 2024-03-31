75880a0e642adb02cffefed994fdcc09cb301c5b
Shadowking912 <96904882+Shadowking912@users.noreply.github.com>
2024-03-31 17:53:55 +0530

    minor edit

--- A2/grpc/fastimport.bash
+++ A2/grpc/fastimport.bash
@@ -5,4 +5,4 @@ sudo apt install git -y
 python3 -m venv venv
 source venv/bin/activate&&pip install grpcio grpcio-tools
 pat="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"
-git clone https://username: $pat @github.com/Shadowking912/dscd.git
+git clone https://username:$pat@github.com/Shadowking912/dscd.git
