334966cc8e60a4e7d5fc28940bf075c7d90a558c
Shadowking912 <96904882+Shadowking912@users.noreply.github.com>
2024-03-31 19:09:50 +0530

    updated bash script

--- A2/grpc/fastimport.sh
+++ A2/grpc/fastimport.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 sudo apt update -y
 sudo apt install python3-pip -y
 sudo apt install python3-venv -y
@@ -6,3 +7,4 @@ python3 -m venv venv
 source venv/bin/activate&&pip install grpcio grpcio-tools
 pat="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"
 git clone https://username:$pat@github.com/Shadowking912/dscd.git
+source venv/bin/activate&&cd dscd/A2/&&chmod -R u+w+x grpc&&cd grpc&&./removeproto.sh 
