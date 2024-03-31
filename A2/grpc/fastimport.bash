4d1d74f47eaf7b6955595c350b8654f704196ad1
Shadowking912 <96904882+Shadowking912@users.noreply.github.com>
2024-03-31 17:47:02 +0530

    restructered folders

---
+++ A2/grpc/fastimport.bash
@@ -0,0 +1,7 @@
+sudo apt update -y
+sudo apt install python3-pip -y
+sudo apt install python3-venv -y
+python3 -m venv venv
+source venv/bin/activate&&pip install grpcio grpcio-tools
+pat="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"
+git clone https://username: $pat @github.com/Shadowking912/dscd.git
--- A2/setup-script.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update -y
-sudo apt install python3-pip -y
-sudo apt install python3-venv -y
-python3 -m venv venv
-source venv/bin/activate&&pip install grpcio grpcio-tools
