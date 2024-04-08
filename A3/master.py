ff32535b8141b85f03b33ca3759c0d64b5bc7702
Shivesh Gulati <72926374+ShiveshGit@users.noreply.github.com>
2024-04-09 00:36:20 +0530

    Started With basic communication
    Co-Authored-By: Shadowking912 <96904882+Shadowking912@users.noreply.github.com>

--- A3/master.py
+++ A3/master.py
@@ -4,51 +4,100 @@ import master_pb2_grpc
 import os
 import sys
 import multiprocessing
+import signal 
 
+def exithandler():
+    active = multiprocessing.active_children()
+    for child in active:
+        child.terminate()
 
+signal.signal(signal.SIGINT, exithandler)
 
-class MapperReducerCommunication(master_pb2_grpc.MapperReducerCommunicationServicer):
+# class MapperReducerCommunication(master_pb2_grpc.MapperReducerCommunicationServicer):
     
-    def PartitionParameters(self, request, context):
-        return super().PartitionParameters(request, context)
+#     def PartitionParameters(self, request, context):
+#         return super().PartitionParameters(request, context)
     
-class MasterMapperCommunication(master_pb2_grpc.MasterMapperCommunicationServicer):
-    def MapperParameters(self, request, context):
-        return super().MapperParameters(request, context)
+# # class MasterMapperCommunication(master_pb2_grpc.MasterMapperCommunicationServicer):
+# #     def MapperParameters(self, request, context):
+# #         return super().MapperParameters(request, context)
 
-class MasterReducerCommunication(master_pb2_grpc.MasterReducerCommunicationServicer):
-    def ReducerParameters(self, request, context):
-        return super().ReducerParameters(request, context)
+# class MasterReducerCommunication(master_pb2_grpc.MasterReducerCommunicationServicer):
+#     def ReducerParameters(self, request, context):
+#         return super().ReducerParameters(request, context)
     
-
-
-
-
-
-
-
-
+    
 def run_reducer(reducer_id):
     # Function to run reducer process
-    print(f"Reducer {reducer_id} started.")
-    # Add your reducer logic here
-    return
+    # command=f"5556{reducer_id}"
+    # exec(open('reducer.py').read(),{'argv':command})
+    os.system("python reducer.py")
+
+
 def run_mapper(mapper_id):
     # Function to run reducer process
-    print(f"Mapper {mapper_id} started.")
-    # Add your reducer logic here
-    return
+    # Assuming the script you want to run is named 'another_script.py'
+    print("mapper ",mapper_id)
+    os.system(f"python mapper.py 5555{mapper_id}")
+
+def read_file():
+    with open(os.path.join(os.getcwd(),f'Data/Input/points.txt')) as f:
+        points = f.readlines()
+
+        
+    final_points = []
+    for point in points:
+        fpoint = point.strip()
+        x,y = fpoint.split(",")
+        final_points.append(tuple([float(x),float(y)]))
+    return final_points
+
+def create_partitions(points,shard_size,num_mappers):
+    # if os.path.isdir(os.path.join(os.getcwd(),f'Data/Splits'))==False:
+    #     os.makedirs(os.path.join(os.getcwd(),f'Data/Splits'))
+    # else:
+    #     os.rmdir(os.path.join(os.getcwd(),f'Data/Splits'))
+    #     os.makedirs(os.path.join(os.getcwd(),f'Data/Splits'))
+
+    split_number = 0
+    num_partitions = len(points)//shard_size
+    if len(points)%shard_size!=0:
+        remaining_size = len(points)-num_partitions*shard_size
+        num_partitions+=1
+    print("Num Partitions = ",num_partitions)
+    
+    print("Remaining Size = ",remaining_size)
+    partition_id = 0
+    start_index = 0
+    partitions=[]
+    for i in range(num_partitions):
+        if start_index+shard_size<=len(points):
+            partitions.append(tuple([start_index,start_index+shard_size]))
+            start_index+=shard_size
+        else:
+            print("Start Index = ",start_index)
+            print("Ending Index = ",start_index+remaining_size)
+            partitions.append(tuple([start_index,start_index+remaining_size]))
+            start_index+=remaining_size
+
+    mapper_partitions={}
+    for i in range(len(partitions)):
+        if i%num_mappers not in mapper_partitions.keys():
+            mapper_partitions[i%num_mappers]=[]
+        mapper_partitions[i%num_mappers].append(partitions[i]) 
+    return mapper_partitions
 
 
 def main():
-    if len(sys.argv) != 5:
-        print("Usage: python master.py <number_of_mappers> <number_of_reducers> <number_of_centroids> <number_of_iterations>")
+    if len(sys.argv) != 6:
+        print("Usage: python master.py <shard_size> <number_of_mappers> <number_of_reducers> <number_of_centroids> <number_of_iterations>")
         sys.exit(1)
 
-    num_mappers = int(sys.argv[1])
-    num_reducers = int(sys.argv[2])
-    num_centroids = int(sys.argv[3])
-    num_iterations = int(sys.argv[4])
+    shard_size = int(sys.argv[1])
+    num_mappers = int(sys.argv[2])
+    num_reducers = int(sys.argv[3])
+    num_centroids = int(sys.argv[4])
+    num_iterations = int(sys.argv[5])
 
     print("Master started.")
     print(f"Number of mappers: {num_mappers}")
@@ -56,30 +105,67 @@ def main():
     print(f"Number of centroids: {num_centroids}")
     print(f"Number of iterations: {num_iterations}")
 
+    # Read the points from the file
+    points = read_file()
+    print(points)    
+    print(len(points))
+
+    mapper_partitions=create_partitions(points,shard_size,num_mappers)
+    print(f"Partitions = ")
+    print(mapper_partitions)
+    print(f"Partitions = {mapper_partitions}")
+
     # Fork for the number of mappers
+
+    print("Mappers started")
     pidListMappers = []
+    
     for mapper_id in range(num_mappers):
         process = multiprocessing.Process(target=run_mapper, args=(mapper_id,))
         pidListMappers.append(process)
         process.start()
         
-    
+    channels=[]
+    for i in range(num_mappers):
+        channel = grpc.insecure_channel(f'localhost:5555{i}')
+        channels.append(channel)
+        stub = master_pb2_grpc.MasterMapperCommunicationStub(channel)
+        request=master_pb2.MapRequest()
+        indices=master_pb2.index()
+        
+        for j in mapper_partitions[i]:
+            indices.startIndex=j[0]
+            indices.endIndex=j[1]
+            print(indices)
+            request.Indices.append(indices)
+        
+        centroids=master_pb2.DataPoint()
+        
+        for i in range(num_centroids):
+            centroids.x=0
+            centroids.y=0
+            print(centroids)
+            request.CentroidCoordinates.append(centroids)
+        response = stub.MapperParameters(request)
+        # response = response.result()
+
     for process in pidListMappers:
         process.join()
 
     print("All Mapper finished.")
 
     # Fork for the number of reducers
-    pidListReducers = []
-    for reducer_id in range(num_reducers):
-        process = multiprocessing.Process(target=run_reducer, args=(reducer_id,))
-        pidListReducers.append(process)
-        process.start()
+    # print("Reducers Started")
+    # pidListReducers = []
+    # for reducer_id in range(num_reducers):
+    #     process = multiprocessing.Process(target=run_reducer, args=(reducer_id,))
+    #     pidListReducers.append(process)
+    #     process.start()
         
-    for process in pidListReducers:
-        process.join()
-
-    print("All reducers are finished.")
+    # for process in pidListReducers:
+    #     process.join()
+    
+    # print("All reducers are  finished.")
 
 if __name__ == "__main__":
     main()
